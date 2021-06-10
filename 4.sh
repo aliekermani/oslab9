d4a8445649890064ff31c2f5a2d9d24fa3ca67c8
aliekermani <83701926+aliekermani@users.noreply.github.com>
2021-06-10 14:14:44 +0430

    kermani

---
+++ 4.sh
@@ -0,0 +1,5 @@
+sudo groupadd uni 
+sudo groupadd sajjad
+sudo adduser oslab sajjad
+sudo adduser oslab uni	
+sudo gpassw -A oslab sajjad
---
+++ 5.sh
@@ -0,0 +1,3 @@
+sudo useradd os2
+sudo usermod -G sajjad os2
+sudo userdel os2
